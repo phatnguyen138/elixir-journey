3bc19765b4bef29824f0a03a0796c7622b74cbd5
Phat <patrick.nguyen.goldenowl@gmail.com>
2025-07-21 22:44:00 +0700

    init project

---
+++ cards/lib/cards.ex
@@ -0,0 +1,18 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def create_deck do
+    [1,2,"Me"]
+  end
+end
