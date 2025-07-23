009556081df631d12d7740120c354841be44fe62
Phat <patrick.nguyen.goldenowl@gmail.com>
2025-07-23 21:46:46 +0700

    lesson list

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,24 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
   def create_deck do
-    [1,2,"Me"]
+    values = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+     "Jack", "Queen", "King", "Ace"]
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+    cards = for suit <- suits  do
+      for value <- values  do
+        "#{value} of #{suit}"
+      end
+    end
+
+    List.flatten(cards)
   end
+
+  def shuffle(deck \\ ["Hehe"]) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
 end
