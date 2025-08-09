a46dadcbdd492d0d0a73cc0ec8890d3ed4f8679e
Phat <minhphatnguyen6@gmail.com>
2025-08-09 13:51:02 +0700

    chunk and mirror

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,8 +6,35 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash
+    |> pick_color
+    |> build_grid
   end
 
+  def build_grid(%Identicon.Image{hex: hex, color: _color} = _image) do
+    hex
+    |> Enum.chunk_every(3,3,:discard)
+    |> Enum.map(&Identicon.mirror/1)
+  end
+
+  def mirror(row) do
+    [first, second | _left] = row
+    row ++ [second, first]
+  end
+
+  @doc """
+  Use for pick RBG color from hex list
+  """
+  def pick_color(%Identicon.Image{hex: [r, g, b | _rest]} = image) do
+    %Identicon.Image{image | color: [r, g, b]}
+  end
+
+
+  @doc """
+  This function return a struct of image struct with hex list from a string
+
+  ## Example
+      iex> Identicon.hash("string")
+  """
   def hash(input) do
     hashed_input = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -1,3 +1,3 @@
 defmodule Identicon.Image do
-  defstruct hex: []
+  defstruct hex: nil, color: nil
 end
